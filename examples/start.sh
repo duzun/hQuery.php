3dc2879738f92850b5969e61c35a36b739a58fca
Dumitru Uzun <contact@duzun.me>
2024-09-29 19:07:28 +0300

    Update to gulp@5 and auto-install composer deps in ./examples folder

--- examples/start.sh
+++ examples/start.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-cd "$(dirname "$0")" && \
-php -S localhost:8000
+cd "$(dirname "$0")" && (
+    if [ ! -s 'vendor/autoload.php' ]; then
+        if command -v composer > /dev/null; then
+            echo "composer install..."
+            composer install --optimize-autoloader
+        else
+            >&2 echo "Please, install composer and run \`compose install\` in $PWD!"
+        fi
+    fi
+
+    php -S localhost:8000
+)
--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,37 @@
 const gulp = require('gulp'),
       php = require('gulp-connect-php'),
       browserSync = require('browser-sync');
+const path = require('path');
+const fs = require('fs');
+const { spawn } = require('node:child_process');
 
-var reload  = browserSync.reload;
+var reload = browserSync.reload;
 
 // - gulp live - start a PHP web-server and browsersync for live-reload
 
-gulp.task('php', function() {
-    php.server({ base: './examples', port: 8010, keepalive: true});
+gulp.task('php', function () {
+    const base = './examples';
+    php.server({ base, port: 8010, keepalive: true });
+
+    if (!fs.existsSync(path.join(base, 'vendor/autoload.php'))) {
+        setTimeout(() => {
+            console.log(`composer install...\n`);
+            const composerInstall = spawn('composer', ['install', '--optimize-autoloader'], { cwd: base });
+            const stderr = [];
+            composerInstall.stderr.on('data', (data) => {
+                stderr.push(data);
+            });
+            composerInstall.on('close', (code) => {
+                console.log(`composer install done, code=${code}`);
+                if (code && stderr.length) {
+                    console.error(`composer install errors:\n${stderr.join('')}`);
+                }
+            });
+        }, 1e3);
+    }
 });
 
-gulp.task('browser-sync', gulp.parallel('php', function() {
+gulp.task('browser-sync', gulp.parallel('php', function () {
     browserSync({
         proxy: '127.0.0.1:8010',
         port: 8080,
@@ -32,5 +53,5 @@ gulp.task('default', gulp.parallel('browser-sync', function () {
             reload();
             watcher.unwatch(path);
         })
-    ;
+        ;
 }));
